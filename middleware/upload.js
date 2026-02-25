d17654142224cbd95ce0e60a3f7d97829b4eda20
unknown <rupalisarcar@gmail.com>
2026-02-25 20:25:45 +0530

    remove comma

--- middleware/upload.js
+++ middleware/upload.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+    limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
 })
 
 module.exports = upload;
